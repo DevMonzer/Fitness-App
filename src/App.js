97d2f299d3eed2d04bf94d1f6621cff9292377cb
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-13 17:20:22 +0300

    added navbar and exercise components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
 
 import "./App.css";
 
-const App = () => <div>App</div>;
+const App = () => (
+  <Box width="400px">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+  </Box>
+);
 
 export default App;
