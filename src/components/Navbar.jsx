d5e5f53b7759e9911eea18a3aec8589f70604989
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-13 17:31:38 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Stack } from "@mui/material";
+
+import Logo from "../assets/images/Logo.png";
 
 export default class Navbar extends Component {
   render() {
-    return <div>Navbar</div>;
+    return (
+      <Stack>
+        <Link to="/">
+          <img
+            src={Logo}
+            alt="Logo"
+            style={{ width: "48px", height: "48px", margin: "0 20px" }}
+          />
+        </Link>
+        <Stack>
+          <Link to="/">Home</Link>
+          <a
+            href="#exercises"
+            style={{ textDecoration: "none", color: "#3A1212" }}
+          >
+            Exercises
+          </a>
+        </Stack>
+      </Stack>
+    );
   }
 }
