2cb0ae932c93ff9b7ff439bc7e696535230f56ff
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-13 19:03:28 +0300

    Update fetchData.js

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,12 @@
+export const options = {
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  headers: {
+    "X-RapidAPI-Key": "e90a4b6292msh085108ca2842138p17a80ajsndd281e5fb251",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+
 export const fetchData = async (url, options) => {
   const res = await fetch(url, options);
   const data = await res.json();
