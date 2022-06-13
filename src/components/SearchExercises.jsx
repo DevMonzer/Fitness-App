bc3f9d6f6cace8c37281c3db2eeee25c69debc68
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-13 19:12:07 +0300

    Update SearchExercises.jsx

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -5,20 +5,29 @@ import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 function SearchExercises() {
   const [search, setSearch] = useState("");
-  const [bodyParts, setBodyParts] = useState([]);
+  const [exercises, setExercises] = useState([]);
 
-  useEffect(() => {
-    const fetchExercisesData = async () => {
-      const bodyPartsData = await fetchData(
-        "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
+  const handleSearch = async () => {
+    if (search) {
+      const exercisesData = await fetchData(
+        "https://exercisedb.p.rapidapi.com/exercises",
         exerciseOptions
       );
 
-      setBodyParts(["all", ...bodyPartsData]);
-    };
+      const searchedExercises = exercisesData.filter(
+        (item) =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
+      );
 
-    fetchExercisesData();
-  }, []);
+      window.scrollTo({ top: 1800, left: 100, behavior: "smooth" });
+
+      setSearch("");
+      setExercises(searchedExercises);
+    }
+  };
 
   return (
     <Stack alignItems="center" mt="37px" justifyContent="center" p="20px">
