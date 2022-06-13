e29b1f099114e29b18401001706dfbada00b2295
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-13 18:52:34 +0300

    Update SearchExercises.jsx

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Box, Button, Stack, TextField, Typography } from "@mui/material";
 
 function SearchExercises() {
   return <div>SearchExercises</div>;
