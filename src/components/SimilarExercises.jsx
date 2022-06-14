ca3b06a6c3c91ad8b5d897c9f6843bc18255a24d
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 12:48:41 +0300

    Update SimilarExercises.jsx

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Typography, Box, Stack } from "@mui/material";
 
-const SimilarExercises = () => {
+import HorizontalScrollbar from "./HorizontalScrollbar";
+import Loader from "./Loader";
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
     <Box sx={{ mt: { lg: "100px", xs: "0px" } }}>
       <Typography
@@ -16,6 +19,13 @@ const SimilarExercises = () => {
         </span>{" "}
         exercises
       </Typography>
+      <Stack direction="row" sx={{ p: 2, position: "relative" }}>
+        {targetMuscleExercises.length !== 0 ? (
+          <HorizontalScrollbar data={targetMuscleExercises} />
+        ) : (
+          <Loader />
+        )}
+      </Stack>
     </Box>
   );
 };
