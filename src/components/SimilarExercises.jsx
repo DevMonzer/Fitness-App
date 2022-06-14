2782da6c2072b03ec6e5075bbb70994197560555
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 12:47:57 +0300

    Update SimilarExercises.jsx

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -2,7 +2,22 @@ import React from "react";
 import { Typography, Box, Stack } from "@mui/material";
 
 const SimilarExercises = () => {
-  return <Box sx={{ mt: { lg: "100px", xs: "0px" } }}></Box>;
+  return (
+    <Box sx={{ mt: { lg: "100px", xs: "0px" } }}>
+      <Typography
+        sx={{ fontSize: { lg: "44px", xs: "25px" }, ml: "20px" }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Similar{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          Target Muscle
+        </span>{" "}
+        exercises
+      </Typography>
+    </Box>
+  );
 };
 
 export default SimilarExercises;
