39d302e22dc03dd6196871d32ecbd8d5d27596ca
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 12:46:41 +0300

    Update SimilarExercises.jsx

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
 
 const SimilarExercises = () => {
   return <div>SimilarExercises</div>;
