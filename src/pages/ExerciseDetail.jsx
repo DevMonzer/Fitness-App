51eb8afbfd878c9abe598743440f0f8bb54414f5
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 12:45:34 +0300

    Update ExerciseDetail.jsx

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,18 +2,16 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
 import Detail from "../components/Detail";
 import ExerciseVideos from "../components/ExerciseVideos";
 import SimilarExercises from "../components/SimilarExercises";
 
-import { exerciseOptions, fetchData, youtubeOptions } from "../utils/fetchData";
-
-function ExerciseDetail() {
+const ExerciseDetail = () => {
   const [exerciseDetail, setExerciseDetail] = useState({});
   const [exerciseVideos, setExerciseVideos] = useState([]);
   const [targetMuscleExercises, setTargetMuscleExercises] = useState([]);
   const [equipmentExercises, setEquipmentExercises] = useState([]);
-
   const { id } = useParams();
 
   useEffect(() => {
@@ -52,6 +50,8 @@ function ExerciseDetail() {
     fetchExercisesData();
   }, [id]);
 
+  if (!exerciseDetail) return <div>No Data</div>;
+
   return (
     <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
       <Detail exerciseDetail={exerciseDetail} />
@@ -59,9 +59,12 @@ function ExerciseDetail() {
         exerciseVideos={exerciseVideos}
         name={exerciseDetail.name}
       />
-      <SimilarExercises />
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
     </Box>
   );
-}
+};
 
 export default ExerciseDetail;
