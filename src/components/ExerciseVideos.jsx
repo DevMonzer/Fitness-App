837927a563ed17af26ae8e3a3100dd0c403d37a7
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 11:03:43 +0300

    Update ExerciseVideos.jsx

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -3,6 +3,8 @@ import { Typography, Box, Stack } from "@mui/material";
 import Loader from "./Loader";
 
 const ExerciseVideos = ({ exerciseVideos, name }) => {
+  if (!exerciseVideos.length) return <Loader />;
+
   return (
     <Box sx={{ marginTop: { lg: "203px", xs: "20px" } }} p="20px">
       <Typography
@@ -11,10 +13,10 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         color="#000"
         mb="33px"
       >
-        Watch
+        Watch{" "}
         <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
           {name}
-        </span>
+        </span>{" "}
         exercise videos
       </Typography>
       <Stack
@@ -22,7 +24,35 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
         justifyContent="flex-start"
         flexWrap="wrap"
         alignItems="center"
-      ></Stack>
+      >
+        {exerciseVideos?.slice(0, 3)?.map((item, index) => (
+          <a
+            key={index}
+            className="exercise-video"
+            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              style={{ borderTopLeftRadius: "20px" }}
+              src={item.video.thumbnails[0].url}
+              alt={item.video.title}
+            />
+            <Box>
+              <Typography
+                sx={{ fontSize: { lg: "28px", xs: "18px" } }}
+                fontWeight={600}
+                color="#000"
+              >
+                {item.video.title}
+              </Typography>
+              <Typography fontSize="14px" color="#000">
+                {item.video.channelName}
+              </Typography>
+            </Box>
+          </a>
+        ))}
+      </Stack>
     </Box>
   );
 };
