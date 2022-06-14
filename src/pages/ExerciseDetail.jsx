8de31540a1f9a81bb798f4c0a0377bed91707f44
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 10:39:14 +0300

    created 3 components for exercises details

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -2,20 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 function ExerciseDetail() {
   return (
     <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
-      <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos
-        exerciseVideos={exerciseVideos}
-        name={exerciseDetail.name}
-      />
-      <SimilarExercises
-        targetMuscleExercises={targetMuscleExercises}
-        equipmentExercises={equipmentExercises}
-      />
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
     </Box>
   );
 }
