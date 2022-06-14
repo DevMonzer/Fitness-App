17301162c4d41712cd26d039cfd1e3ceb10a0dd9
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 10:51:39 +0300

    Update Detail.jsx

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -14,7 +14,15 @@ const Detail = ({ exerciseDetail }) => {
       sx={{ flexDirection: { lg: "row" }, p: "20px", alignItems: "center" }}
     >
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
-      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}></Stack>
+      <Stack sx={{ gap: { lg: "35px", xs: "20px" } }}>
+        <Typography
+          sx={{ fontSize: { lg: "64px", xs: "30px" } }}
+          fontWeight={700}
+          textTransform="capitalize"
+        >
+          {name}
+        </Typography>
+      </Stack>
     </Stack>
   );
 };
