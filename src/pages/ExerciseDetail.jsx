2f4888e848c525e8041858f549e26013c39dfc2e
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 10:37:27 +0300

    Update ExerciseDetail.jsx

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -5,7 +5,19 @@ import { Box } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 function ExerciseDetail() {
-  return <div>ExerciseDetail</div>;
+  return (
+    <Box sx={{ mt: { lg: "96px", xs: "60px" } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={exerciseDetail.name}
+      />
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
+    </Box>
+  );
 }
 
 export default ExerciseDetail;
