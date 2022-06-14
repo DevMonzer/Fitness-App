adad9dd1cbcf22e518fdd2ba4a23419e345ededa
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 11:00:41 +0300

    Update ExerciseVideos.jsx

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Typography, Box, Stack } from "@mui/material";
+import Loader from "./Loader";
 
 const ExerciseVideos = () => {
   return <div>ExerciseVideos</div>;
