4e181e6b4687f34f1a464acf331e9ecc2f864e7f
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-14 12:49:42 +0300

    Update SimilarExercises.jsx

--- src/components/SimilarExercises.jsx
+++ src/components/SimilarExercises.jsx
@@ -13,10 +13,10 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
         color="#000"
         mb="33px"
       >
-        Similar{" "}
+        Similar
         <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
           Target Muscle
-        </span>{" "}
+        </span>
         exercises
       </Typography>
       <Stack direction="row" sx={{ p: 2, position: "relative" }}>
@@ -26,6 +26,22 @@ const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
           <Loader />
         )}
       </Stack>
+      <Typography
+        sx={{
+          fontSize: { lg: "44px", xs: "25px" },
+          ml: "20px",
+          mt: { lg: "100px", xs: "60px" },
+        }}
+        fontWeight={700}
+        color="#000"
+        mb="33px"
+      >
+        Similar{" "}
+        <span style={{ color: "#FF2625", textTransform: "capitalize" }}>
+          Equipment
+        </span>{" "}
+        exercises
+      </Typography>
     </Box>
   );
 };
