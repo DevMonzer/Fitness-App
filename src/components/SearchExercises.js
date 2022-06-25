51e7017fc836c99359732379e87e337e0e4b9f66
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 19:47:30 +0300

    update search exercise component and fetchdata function

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -6,7 +6,10 @@ import { exerciseOptions, fetchData } from '../utils/fetchData';
 const SearchExercises = () => {
   const [search, setSearch] = useState('');
 
-  const handleSearch = async () => {
+  // Here is the request to the exercises API to get data from
+  const handleSearch = async e => {
+    e.preventDefault();
+
     if (search) {
       const exercisesData = await fetchData(
         'https://exercisedb.p.rapidapi.com/exercises',
@@ -28,35 +31,37 @@ const SearchExercises = () => {
         Awesome Exercises You <br /> Should Know
       </Typography>
       <Box position="relative" mb="72px">
-        <TextField
-          height="76px"
-          sx={{
-            input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
-            width: { lg: '1170px', xs: '350px' },
-            backgroundColor: '#fff',
-            borderRadius: '40px',
-          }}
-          value={search}
-          onChange={e => setSearch(e.target.value.toLocaleLowerCase())}
-          placeholder="Search Exercises"
-          type="text"
-        />
-        <Button
-          className="search-btn"
-          sx={{
-            bgcolor: '#FF2625',
-            color: '#fff',
-            textTransform: 'none',
-            width: { lg: '173px', xs: '80px' },
-            height: '56px',
-            position: 'absolute',
-            right: '0px',
-            fontSize: { lg: '20px', xs: '14px' },
-          }}
-          onClick={handleSearch}
-        >
-          Search
-        </Button>
+        <form onSubmit={handleSearch}>
+          <TextField
+            height="76px"
+            sx={{
+              input: { fontWeight: '700', border: 'none', borderRadius: '4px' },
+              width: { lg: '1170px', xs: '350px' },
+              backgroundColor: '#fff',
+              borderRadius: '40px',
+            }}
+            value={search}
+            onChange={e => setSearch(e.target.value.toLocaleLowerCase())}
+            placeholder="Search Exercises"
+            type="text"
+          />
+          <Button
+            className="search-btn"
+            sx={{
+              bgcolor: '#FF2625',
+              color: '#fff',
+              textTransform: 'none',
+              width: { lg: '173px', xs: '80px' },
+              height: '56px',
+              position: 'absolute',
+              right: '0px',
+              fontSize: { lg: '20px', xs: '14px' },
+            }}
+            onClick={handleSearch}
+          >
+            Search
+          </Button>
+        </form>
       </Box>
     </Stack>
   );
