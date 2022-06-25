333887918b26cbd1b41cfc70b4abf1118558252b
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 19:47:57 +0300

    Update SearchExercises.js

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -16,7 +16,18 @@ const SearchExercises = () => {
         exerciseOptions
       );
 
-      console.log(exercisesData);
+      const searchedExercises = exercisesData.filter(
+        item =>
+          item.name.toLowerCase().includes(search) ||
+          item.target.toLowerCase().includes(search) ||
+          item.equipment.toLowerCase().includes(search) ||
+          item.bodyPart.toLowerCase().includes(search)
+      );
+
+      window.scrollTo({ top: 1800, left: 100, behavior: 'smooth' });
+
+      setSearch('');
+      setExercises(searchedExercises);
     }
   };
 
