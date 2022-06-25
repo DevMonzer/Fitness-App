dd058dbdb7c3e5f539976f07a6341b9fe75432f6
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 21:06:40 +0300

    Update BodyPart.js

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -3,6 +3,25 @@ import { Stack, Typography } from '@mui/material';
 
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = () => <div>BodyPart</div>;
+const BodyPart = ({ item, setBodyPart, bodyPart }) => (
+  <Stack
+    type="button"
+    alignItems="center"
+    justifyContent="center"
+    className="bodyPart-card"
+  >
+    <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px' }} />
+    <Typography
+      fontSize="24px"
+      fontWeight="bold"
+      fontFamily="Alegreya"
+      color="#3A1212"
+      textTransform="capitalize"
+    >
+      {' '}
+      {item}
+    </Typography>
+  </Stack>
+);
 
 export default BodyPart;
