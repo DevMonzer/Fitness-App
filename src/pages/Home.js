9dd3ec86b9fd40a950058b7144e4fd8f6cbf22d3
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 17:00:23 +0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        exercises={exercises}
+        bodyPart={bodyPart}
+      />
+    </Box>
+  );
 };
 
 export default Home;
