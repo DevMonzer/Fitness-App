2c69072f149663227bce1447a17816469fe15045
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 16:56:06 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,6 +14,24 @@ const Navbar = () => {
           style={{ width: '48px', height: '48px', margin: '0px 20px' }}
         />
       </Link>
+      <Stack>
+        <Link
+          to="/"
+          style={{
+            textDecoration: 'none',
+            color: '#3A1212',
+            borderBottom: '3px solid #FF2625',
+          }}
+        >
+          Home
+        </Link>
+        <a
+          href="#exercises"
+          style={{ textDecoration: 'none', color: '#3A1212' }}
+        >
+          Exercises
+        </a>
+      </Stack>
     </Stack>
   );
 };
