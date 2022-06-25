93262aa4accdbfcd370b283b7f8d1c2ec5302400
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 17:02:07 +0300

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/banner.jpg';
 
 const HeroBanner = () => <div>HeroBanner</div>;
 
