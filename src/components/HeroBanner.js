0c4318806b75d661722c91a90165d42016d641aa
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 17:05:44 +0300

    Update HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -24,6 +24,25 @@ const HeroBanner = () => (
     <Typography fontSize="22px" fontFamily="Alegreya" lineHeight="35px">
       Check out the most effective exercises personalized to you
     </Typography>
+    <Stack>
+      <a
+        href="#exercises"
+        style={{
+          marginTop: '45px',
+          textDecoration: 'none',
+          width: '200px',
+          textAlign: 'center',
+          background: '#FF2625',
+          padding: '14px',
+          fontSize: '22px',
+          textTransform: 'none',
+          color: 'white',
+          borderRadius: '4px',
+        }}
+      >
+        Explore Exercises
+      </a>
+    </Stack>
   </Box>
 );
 
