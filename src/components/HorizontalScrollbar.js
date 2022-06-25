32242ce95f17035b8f6a58cab82c030a5b949b75
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 21:13:48 +0300

    update BodyPart, and HorizontalScrollbar components

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -2,10 +2,33 @@ import React, { useContext } from 'react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
 
+import RightArrowIcon from '../assets/icons/right-arrow.png';
+import LeftArrowIcon from '../assets/icons/left-arrow.png';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollPrev()} className="right-arrow">
+      <img src={LeftArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Typography onClick={() => scrollNext()} className="left-arrow">
+      <img src={RightArrowIcon} alt="right-arrow" />
+    </Typography>
+  );
+};
+
 import BodyPart from './BodyPart';
 
 const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
-  console.log(data);
+  // console.log(data);
   return (
     <div>
       {data.map(item => {
@@ -15,6 +38,7 @@ const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
           title={item.id || item}
           m="0 40px"
         >
+          {console.log(item)}
           <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} />
         </Box>;
       })}
