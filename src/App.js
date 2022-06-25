fda81d319c34a742d4220354b6d6ddb0b4d084fa
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-25 16:49:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import { Box } from '@mui/material';
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
