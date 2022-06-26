1c45f466a560a4e01028f948c845c8c869930662
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-26 10:01:21 +0300

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -6,23 +6,26 @@ import ExerciseCard from './ExerciseCard';
 
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 
-// Pagination
-const indexOfLastExercise = currentPage * exercisesPerPage;
-const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
-const currentExercises = exercises.slice(
-  indexOfFirstExercise,
-  indexOfLastExercise
-);
-
-const paginate = (event, value) => {
-  setCurrentPage(value);
-
-  window.scrollTo({ top: 1800, behavior: 'smooth' });
-};
-
-if (!currentExercises.length) return;
-
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [exercisesPerPage] = useState(6);
+
+  // Pagination
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise
+  );
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+
+    window.scrollTo({ top: 1800, behavior: 'smooth' });
+  };
+
+  if (!currentExercises.length) console.log('ss');
+
   return (
     <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
       <Typography
