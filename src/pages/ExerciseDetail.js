61d8d3fe781cf23a5c368b301d2e846d5a1ab8c3
Monzer Mohammad <dev.monzer@gmail.com>
2022-06-26 09:53:47 +0300

    Update ExerciseDetail.js

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
 
 const ExerciseDetail = () => {
   return <div>ExerciseDetail</div>;
